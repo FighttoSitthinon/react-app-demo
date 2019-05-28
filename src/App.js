6eea67001c7b33504ea17380fff1cc2e189343dc
Sitthinon Chanaritthichai <sitthinon.c@hotmail.com>
2019-05-28 12:55:28 +0700

    use setState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Hello from '../src/component/helloComponent';
-import Counter from '../src/component/countComponent';
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Hello from "../src/component/helloComponent";
+import Counter from "../src/component/countComponent";
 
 function App() {
+  const [isBool, setBool] = useState(false);
+
+  const changeState = () => {
+    setBool(!isBool);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Hello/>
+        <Hello />
       </header>
-      <Counter/>
+      <span className="m-2">{isBool ? "true" : "false"}</span>
+      <button onClick={changeState}>Set bool</button>
+      <Counter />
     </div>
   );
-
-  
 }
 
 export default App;
